1c7abb801b365d6c6fd56c57cc4924136ef040dc
Boğaçhan Yiğitbaşı <bogachanyigitbasi@Bogachans-MacBook-Air.local>
2022-11-14 18:48:53 +0300

    test init is completed

---
+++ README.md
@@ -0,0 +1,7 @@
+**auction contract** as given in the example
+1- Anyboyd can bid.
+2- Contract takes the highest bid always.
+3- It ensures the given time is expired or not.
+4- Finalizes the auction.
+5- It has some vulnerabilities.
+6- returns back the old bid if a new one placed.
--- src/lib.rs
+++ src/lib.rs
@@ -56,6 +56,7 @@ struct InitParameter {
 }
 
 // special errors
+#[derive(Debug, PartialEq, Eq, Clone, Reject, Serial, SchemaType)]
 enum BidError {
     OnlyAccount,               // contracts cant bid
     BidMore,                   // only higher bids accepted, raised when amount is low
@@ -64,7 +65,7 @@ enum BidError {
 }
 
 // finalize function errors
-#[derive(Debug, PartialEq, Eq, Clone, Reject, SchemaType)]
+#[derive(Debug, PartialEq, Eq, Clone, Reject, Serial, SchemaType)]
 enum FinalizeError {
     AuctionStillActive, // raised when owner tries to finalize before it's end time
     AuctionAlreadyFinalized, // raised when trying to finalize already finalized one
@@ -96,13 +97,7 @@ fn auction_init<S: HasStateApi>(
 }
 //receive = accepts input from outside
 // contract name, function name to invoke
-#[receive(
-    contract = "auction",
-    name = "bid",
-    payable,
-    mutable,
-    error = "BidError"
-)]
+#[receive(contract = "auction", name = "bid", payable, mutable)]
 fn auction_bid<S: HasStateApi>(
     ctx: &impl HasReceiveContext,
     host: &mut impl HasHost<State, StateApiType = S>,
@@ -112,15 +107,15 @@ fn auction_bid<S: HasStateApi>(
     ensure_eq!(
         host.state_mut().auction_state,
         AuctionState::Continue,
-        BidError::AuctionAlreadyFinalized
+        BidError::AuctionFinalizedButBidded
     );
     // not filter user, everybody can bid maybe except for the owner
-    ensure_eq!(ctx.sender, ctx.owner, BlacklistedBidder::Blacklisted);
+    // ensure_eq!(ctx.sender, ctx.owner, BidError::Blacklisted);
 
     // check time when bid arrives and auction still continue
     let slot_time = ctx.metadata().slot_time();
 
-    ensure!(slot_time <= host.state_mut().end(), BidError::BidTooLate);
+    ensure!(slot_time <= host.state_mut().end, BidError::BidTooLate);
 
     // ensure only accounts can bid not contracts
     let sender_address = match ctx.sender() {
@@ -161,7 +156,122 @@ fn view_highest_bid<S: HasStateApi>(
     Ok(host.self_balance())
 }
 
-// finalize the auction
+// finalize the auction, send the highest bid to the contract owner
+// of the contract instance. In the next version there will be NFT transfer
+// to the highest bidder.
+
+#[receive(contract = "auction", name = "finalize", mutable)]
+fn auction_finalize<S: HasStateApi>(
+    ctx: &impl HasReceiveContext,
+    host: &mut impl HasHost<State, StateApiType = S>,
+) -> Result<(), FinalizeError> {
+    let state = host.state();
+    // ensure auction still continues
+
+    ensure_eq!(
+        state.auction_state,
+        AuctionState::Continue,
+        FinalizeError::AuctionAlreadyFinalized
+    );
+
+    let slot_time = ctx.metadata().slot_time();
+    // Ensure the auction has ended already
+    ensure!(slot_time > state.end, FinalizeError::AuctionStillActive);
+
+    if let Some(account_address) = state.highest_bidder {
+        // mark the auction end
+        host.state_mut().auction_state = AuctionState::Sold(account_address);
+        let owner = ctx.owner();
+
+        let balance = host.self_balance(); // contract balance
+        host.invoke_transfer(&owner, balance).unwrap_abort();
+    }
+    Ok(())
+}
+
+#[concordium_cfg_test]
+mod tests {
+    use super::*;
+    use std::sync::atomic::{AtomicU8, Ordering};
+    use test_infrastructure::*;
+
+    // a counter for generating new accounts
+    static ADDRESS_COUNTER: AtomicU8 = AtomicU8::new(0);
+    const AUCTION_END: u64 = 1;
+    const ITEM: &str = "Starry night by Van Gogh";
+
+    fn expect_error<E, T>(expr: Result<T, E>, err: E, msg: &str)
+    where
+        E: Eq + Debug,
+        T: Debug,
+    {
+        let actual = expr.expect_err_report(msg);
+        claim_eq!(actual, err)
+    }
+
+    fn item_and_param() -> InitParameter {
+        InitParameter {
+            item: ITEM.into(),
+            end: Timestamp::from_timestamp_millis(AUCTION_END),
+        }
+    }
+
+    fn create_parameter_bytes(parameter: &InitParameter) -> Vec<u8> {
+        to_bytes(parameter)
+    }
+
+    fn parametrized_init_ctx(parameter_bytes: &[u8]) -> TestInitContext {
+        let mut ctx = TestInitContext::empty();
+        ctx.set_parameter(parameter_bytes);
+        ctx
+    }
+
+    fn new_account() -> AccountAddress {
+        let account = AccountAddress([ADDRESS_COUNTER.load(Ordering::SeqCst); 32]);
+        ADDRESS_COUNTER.fetch_add(1, Ordering::SeqCst);
+        account
+    }
+
+    fn new_account_ctx<'a>() -> (AccountAddress, TestReceiveContext<'a>) {
+        let account = new_account();
+        let ctx = new_ctx(account, account, AUCTION_END);
+        (account, ctx)
+    }
+
+    fn new_ctx<'a>(
+        owner: AccountAddress,
+        sender: AccountAddress,
+        slot_time: u64,
+    ) -> TestReceiveContext<'a> {
+        let mut ctx = TestReceiveContext::empty();
+        ctx.set_sender(Address::Account(sender));
+        ctx.set_owner(owner);
+        ctx.set_metadata_slot_time(Timestamp::from_timestamp_millis(slot_time));
+        ctx
+    }
+
+    fn bid(
+        host: &mut TestHost<State>,
+        ctx: &TestContext<TestReceiveOnlyData>,
+        amount: Amount,
+        current_contract_balance: Amount,
+    ) {
+        //set balance
+        // initial + bid
+        host.set_self_balance(amount + current_contract_balance);
+
+        auction_bid(ctx, host, amount).expect_report("Bidding should pass");
+    }
+
+    #[concordium_test]
+    fn test_init() {
+        let parameter_bytes = create_parameter_bytes(&item_and_param());
+        let ctx = parametrized_init_ctx(&parameter_bytes);
+        let mut state_builder = TestStateBuilder::new();
+        let state_result = auction_init(&ctx, &mut state_builder);
+        state_result.expect_report("Contract initialize error");
+    }
+}
 
 // #[cfg(test)]
 // mod tests {
